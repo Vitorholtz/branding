fd88bee2ef3fc16269e5087490f9afff620d9d1a
Vítor Holtz <vitorholtz27@gmail.com>
2021-02-24 12:03:53 -0300

    sections

--- main.js
+++ main.js
@@ -9,3 +9,20 @@ hamburgerToggle.addEventListener('click', () => {
     mobileMenu.classList.toggle('mobile-menu-active');
 
 })
+
+// to-the-top icon
+
+window.onscroll = function () {
+    let currentScrollPos = window.pageYOffset;
+    let position = 1082;
+
+    if (currentScrollPos >= position) {
+        document.getElementById("stickhome_img").style.opacity = 1;
+        document.getElementById("stickhome_img").style.visibility = 'visible';
+        document.getElementById("stickhome_img").style.top = '50%';
+    } else {
+        document.getElementById("stickhome_img").style.opacity = 0;
+        document.getElementById("stickhome_img").style.visibility = 'hidden';
+        document.getElementById("stickhome_img").style.top = '0em';
+    }
+}
